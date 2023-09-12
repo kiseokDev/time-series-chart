da93dbe8cc154bcb74115a2e88bf322fb3072297
kiseokDev <dev.leekiseok@gmail.com>
2023-09-12 13:50:00 +0900

    FEAT: hover시 데이터 튤팁제공 기능 구현

--- src/component/Chart.tsx
+++ src/component/Chart.tsx
@@ -1,4 +1,6 @@
 import { useChart } from 'hooks/useChart';
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 
 const Chart = () => {
   const { data, uniqueIds, filtered, handleFilterById } = useChart();
@@ -21,14 +23,21 @@ const Chart = () => {
         {Object.keys(data).map(date => {
           const chartData = data[date];
           const isFiltered = filtered.has(date);
+          const tooltipContent = `${chartData.id},${chartData.value_area},${chartData.value_bar}`;
           return (
             <li
               key={date}
+              data-tooltip-id={`tooltip-${date}`}
               onClick={() => handleFilterById(chartData.id)}
               style={{ color: isFiltered ? 'red' : 'black' }}
             >
               날짜: {date}, 구: {chartData.id}, 면적 값: {chartData.value_area},
               막대 값: {chartData.value_bar}
+              <Tooltip
+                id={`tooltip-${date}`}
+                content={tooltipContent}
+                place="top"
+              />
             </li>
           );
         })}
